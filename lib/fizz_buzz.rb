9d9010d65ea7593c14586e88dab9e37e42e9d103
yoyakoba <mary.sheep.1231@gmail.com>
2019-03-29 08:23:36 +0000

    Fizz Buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,21 @@
+def fizz_buzz(num)
+  if num !~ /^[0-9]+$/
+    return "整数を入力してください"
+  end
+
+  num = num.to_i
+
+  if num.modulo(15) == 0
+    "FizzBuzz"
+  elsif num.modulo(3) == 0
+    "Fizz"
+  elsif num.modulo(5) == 0
+    "Buzz"
+  else
+    num
+  end
+
+end
+
+puts fizz_buzz(ARGV[0])
+

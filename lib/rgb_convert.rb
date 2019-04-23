8fb1250609d675f55a5692f1348579db355c4129
yoyakoba <mary.sheep.1231@gmail.com>
2019-04-23 08:21:06 +0000

    to_intsリファクタリング

--- lib/rgb_convert.rb
+++ lib/rgb_convert.rb
@@ -5,10 +5,5 @@ def to_hex(r, g, b)
 end
 
 def to_ints(str_hex)
-  r = str_hex[1..2]
-  g = str_hex[3..4]
-  b = str_hex[5..6]
-  [r, g, b].inject([]) { |ints, n|
-    ints << n.hex
-  }
+  str_hex.scan(/\w\w/).map( &:hex )
 end

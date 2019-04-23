975d51a18be2042172a0333fa325d6d22ef1fc0d
yoyakoba <mary.sheep.1231@gmail.com>
2019-04-23 08:04:38 +0000

    to_ints実装

--- lib/rgb_convert.rb
+++ lib/rgb_convert.rb
@@ -4,3 +4,11 @@ def to_hex(r, g, b)
   }
 end
 
+def to_ints(str_hex)
+  r = str_hex[1..2]
+  g = str_hex[3..4]
+  b = str_hex[5..6]
+  [r, g, b].inject([]) { |ints, n|
+    ints << n.hex
+  }
+end
--- test/rgb_convert_test.rb
+++ test/rgb_convert_test.rb
@@ -2,10 +2,16 @@ require 'minitest/autorun'
 require './lib/rgb_convert'
 
 class RgbConvertTest < Minitest::Test
-  def test_rgb_convert
+  def test_to_hex
     assert_equal '#000000', to_hex(0, 0, 0)
     assert_equal '#ffffff', to_hex(255,255,255)
-    assert_equal '#800f08', to_hex(128, 15, 8)  
+    assert_equal '#800f08', to_hex(128, 15, 8)
+  end
+
+  def test_to_ints
+    assert_equal [0, 0, 0], to_ints('#000000')
+    assert_equal [255,255,255], to_ints('#ffffff')
+    assert_equal [128, 15, 8], to_ints('#800f08')
   end
 end
 

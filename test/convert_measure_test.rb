ffe23a75b2678c424bfed529cfd49665a8e81000
yoyakoba <mary.sheep.1231@gmail.com>
2019-04-26 08:44:25 +0000

    例外テスト追加

--- test/convert_measure_test.rb
+++ test/convert_measure_test.rb
@@ -7,6 +7,15 @@ class ConvertMeasureTest < Minitest::Test
     assert_equal 3.28,  convert_measure(1, unit_from: :m, unit_to: :ft)
     assert_equal 0.30,  convert_measure(1, unit_from: :ft, unit_to: :m)
     assert_equal 0.51,  convert_measure(20, unit_from: :in, unit_to: :m)
+
+    # 引数が足りない場合はエラー
+    # (例外のテストってやるのだろうか……？)
+    assert_raises(ArgumentError) do convert_measure(100) end
+    assert_raises(ArgumentError) do convert_measure(100, unit_from: :in) end
+    assert_raises(ArgumentError) do convert_measure(100, unit_to: :in) end
+
+    # 存在しないキーを指定した場合はエラー
+    assert_raises(TypeError) do convert_measure(100, unit_from: :yd, unit_to: :in) end
   end
 end
 

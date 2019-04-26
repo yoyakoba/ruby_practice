60f0197c60ab903d1ceca8d0d052dd787473eee7
yoyakoba <mary.sheep.1231@gmail.com>
2019-04-26 08:16:51 +0000

    換算値の定数化

--- lib/convert_measure.rb
+++ lib/convert_measure.rb
@@ -1,11 +1,13 @@
+# メートル換算した際の値
+# 変動がないため定数にする
+UNITS = {
+  :m  => 1.00,
+  :ft => 3.28,
+  :in => 39.37,
+}
+
 def convert_measure(length, unit_from:, unit_to:)
-  units = {
-    :m  => 1.00,
-    :ft => 3.28,
-    :in => 39.37,
-  }
-  
   # 小数点2位で四捨五入
   # キーワード引数はシンボルではないので :unit_from のようには記述しない
-  (length / units[unit_from] * units[unit_to]).round(2)
+  (length / UNITS[unit_from] * UNITS[unit_to]).round(2)
 end
